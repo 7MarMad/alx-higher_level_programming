6b3e3d39eddda35c88a380eb36d18b45942ff440
madani <marwane.madani99@gmail.com>
2023-10-05 13:26:03 -0700

    fix for pow output

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 def pow(a, b):
     c = a
-    for i in range(b + 1):
+    if b == 0:
+        return 1
+    for i in range(2, b + 1):
         c = c * a
-    return c
+    if b > 0:
+        return c
+    else:
+        return (1 / c)
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def uppercasee(str):
+def uppercase(str):
     for c in str:
         if ord(c) > 96 and ord(c) < 123:
             c = chr(ord(c - 32))
