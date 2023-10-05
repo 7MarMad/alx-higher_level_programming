114d361c1393ee7f1821dd2ccf9d7ee8cd9c83d8
madani <marwane.madani99@gmail.com>
2023-10-05 06:08:30 -0700

    0 and 1st tasks

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+clo_num = number
+sign = 1
+if number < 0:
+    number = -number
+    sign = -1
+    if (number % 10) == 0:
+        print(f"Last digit of {clo_num} is 0 and is 0")
+    else:
+        print(f"Last digit of {clo_num} is {sign *(number % 10)} and is less than 6 and not 0")
+else:
+    if (number % 10) == 0:
+        print(f"Last digit of {clo_num} is 0 and is 0")
+    elif (number % 10) < 6:
+        print(f"Last digit of {clo_num} is {number % 10} and is less than 6 and not 0")
+    elif (number % 10) > 5:
+        print(f"Last digit of {clo_num} is {sign * (number % 10)} and is greater than 5")
