e1c7809f18be33e91a716f69fd9295df0f62a42e
madani <marwane.madani99@gmail.com>
2023-10-05 08:46:56 -0700

    fix for not using string format and using f-strings

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -10,13 +10,13 @@ if number < 0:
         print(f"Last digit of {clo_num} is 0 and is 0")
     else:
         print(f"Last digit of {clo_num} is {sign *(number % 10)}\
-                and is less than 6 and not 0")
+ and is less than 6 and not 0")
 else:
     if (number % 10) == 0:
         print(f"Last digit of {clo_num} is 0 and is 0")
     elif (number % 10) < 6:
         print(f"Last digit of {clo_num} is {number % 10}\
-                and is less than 6 and not 0")
+ and is less than 6 and not 0")
     elif (number % 10) > 5:
         print(f"Last digit of {clo_num} is {sign * (number % 10)}\
-                and is greater than 5")
+ and is greater than 5")
