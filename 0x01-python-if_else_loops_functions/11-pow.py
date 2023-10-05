1f7fbd02071b1b59912e2187889901b2501a537e
madani <marwane.madani99@gmail.com>
2023-10-05 14:56:06 -0700

    fix for pow outcome of a case and 12 task is done

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 def pow(a, b):
     c = a
+    d = b
     if b == 0:
         return 1
-    for i in range(2, b + 1):
+    if b < 0:
+        d = -b
+    for i in range(2, d + 1):
         c = c * a
     if b > 0:
         return c
