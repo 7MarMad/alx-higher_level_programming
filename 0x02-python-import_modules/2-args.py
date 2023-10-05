d2537a7bc8f8ddc3f33d26cf9d4a616ad1f941f6
madani <marwane.madani99@gmail.com>
2023-10-05 16:53:06 -0700

    small  fix the same as previous tasks

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-import sys
-print("{} argument".format(len(sys.argv) - 1), end="")
-if len(sys.argv) == 1:
-    print("s.")
-elif len(sys.argv) == 2:
-    print(":")
-    print("1: {}".format(sys.argv[1]))
-elif len(sys.argv) > 2:
-    print("s:")
-    for i in range(len(sys.argv) - 1):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+if __name__ == "__main__":
+    import sys
+    print("{} argument".format(len(sys.argv) - 1), end="")
+    if len(sys.argv) == 1:
+        print("s.")
+    elif len(sys.argv) == 2:
+        print(":")
+        print("1: {}".format(sys.argv[1]))
+    elif len(sys.argv) > 2:
+        print("s:")
+        for i in range(len(sys.argv) - 1):
+            print("{}: {}".format(i + 1, sys.argv[i + 1]))
