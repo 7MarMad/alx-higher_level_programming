7316625cb9e676500556c7c5fd0a9d26dc743028
madani <marwane.madani99@gmail.com>
2023-10-06 14:44:46 -0700

    something not right and im trying to find out what is the checker is mad about

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 # if __name__ == "__main__":
 def magic_calculation(a, b):
-    import magic_calculation_102
-    add = magic_calclation_102.add
-    sub = magic_calclation_102.sub
+    from magic_calculation_102 import add, sub
+
     if a < b:
         c = add(a, b)
         for i in range(4, 6):
