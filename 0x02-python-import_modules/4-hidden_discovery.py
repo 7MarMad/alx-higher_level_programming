8219cbdfa76558e833f82274303f4dc772e50133
madani <marwane.madani99@gmail.com>
2023-10-05 17:27:46 -0700

    task 4and 5

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4
+    res_dir = dir(hidden_4)
+    i = 0
+    for j in res_dir:
+        if j[0:2] != "__":
+            break
+        i = i + 1
+    for l in range(i, len(res_dir)):
+        print(res_dir[l])
