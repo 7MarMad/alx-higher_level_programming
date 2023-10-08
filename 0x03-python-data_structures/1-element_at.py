4f0b99ae748e921bf87f631127d6cfbac6d52839
madani <marwane.madani99@gmail.com>
2023-10-08 06:53:57 -0700

    getting confused and putting and instead of or in a condition (if)

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,7 +2,7 @@
 
 def element_at(my_list, idx):
     lenn = len(my_list)
-    if idx < 0 and idx >= lenn:
+    if idx < 0 or idx >= lenn:
         return None
     else:
         return my_list[idx]
