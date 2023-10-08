ec27b073120840f91901f00ac09037770b463103
madani <marwane.madani99@gmail.com>
2023-10-08 15:36:31 -0700

    task 4,5,6

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def no_c(my_string):
+    sttr = ""
+    str_list = list(my_string)
+    len1 = len(str_list.count('c'))
+    len2 = len(str_list.count('C'))
+
+    for i in range(len1):
+        str_list.remove('c')
+    for j in range(len2):
+        str_list.remove('C')
+    return sttr.join(str_list)
