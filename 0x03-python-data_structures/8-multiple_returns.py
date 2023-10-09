fc87d5d715113ec13719451e8057c8bb1a200535
madani <marwane.madani99@gmail.com>
2023-10-09 08:22:00 -0700

    fix for task 8 for str being empty, and task 9

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    len1 = len(sentence)
-    char = sentence[0]
+    if sentence:
+        len1 = len(sentence)
+        char = sentence[0]
+    else:
+        len1 = 0
+        char = None
     tup = (len1, char)
     return tup
