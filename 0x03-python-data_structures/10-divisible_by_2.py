056d4104192ba49bc22c889c4d88a65a674a7009
madani <marwane.madani99@gmail.com>
2023-10-09 09:48:34 -0700

    task 10, fix for task 9 if list is empty

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def divisible_by_2(my_list=[]):
+    if my_list:
+        len1 = len(my_list)
+        n_list = [False]* len1
+
+        for i in range(len1):
+            if (my_list[i] % 2 == 0):
+                n_list[i] = True
+        return n_list
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
 def max_integer(my_list=[]):
-    my_list.sort()
-    my_list.reverse()
-    return (my_list[0])
+    if my_list:
+        my_list.sort()
+        my_list.reverse()
+        return (my_list[0])
+    else:
+        return None
