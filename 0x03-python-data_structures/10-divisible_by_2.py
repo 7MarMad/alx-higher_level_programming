5435d3162ebdb04f8f9b41feba8339daec15996a
madani <marwane.madani99@gmail.com>
2023-10-09 10:51:24 -0700

    task 11,12, pycodestyle for 10

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -3,7 +3,7 @@
 def divisible_by_2(my_list=[]):
     if my_list:
         len1 = len(my_list)
-        n_list = [False]* len1
+        n_list = [False] * len1
 
         for i in range(len1):
             if (my_list[i] % 2 == 0):
