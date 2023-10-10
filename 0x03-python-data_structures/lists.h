bf6684ae6d8483b1c27515bff7626e33c6510bc8
madani <marwane.madani99@gmail.com>
2023-10-09 17:13:23 -0700

    mistake of not typing correctly

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,10 +1,6 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
-
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -20,10 +16,9 @@ typedef struct listint_s
 } listint_t;
 
 size_t print_listint(const listint_t *h);
-listint_t add_nodeint_end(listint_t **head, const int n);
+listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 
 int is_palindrome(listint_t **head);
-int num_ofnodes(listint_t **head);
 
 #endif /* LISTS_H */
