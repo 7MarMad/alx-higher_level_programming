473cdc646dc50345c9d371538459d2c1d259c832
madani <marwane.madani99@gmail.com>
2023-10-11 08:56:58 -0700

    task 5, 6

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def print_sorted_dictionary(a_dictionary):
+    dic = {}
+    llist = sorted(a_dictionary)
+    
+    for i in llist:
+        dic[i] = a_dictionary[i]
+    for j, m in dic.items():
+        print("{}: {}".format(j, m))
