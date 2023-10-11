5f52f95bd974cfdab7189c021dd9ddf78d94046a
madani <marwane.madani99@gmail.com>
2023-10-11 12:03:18 -0700

    task 14

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
+    if not m_list:
+        return 0
+
     x, y = 0, 0
     for i in range(len(my_list)):
         x += my_list[i][0] * my_list[i][1]
---
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+def square_matrix_map(matrix=[]):
+    return list(map(lambda x: x**2, matrix[0], matrix[1], matrix[2]))
