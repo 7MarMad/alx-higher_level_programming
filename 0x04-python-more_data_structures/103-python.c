903b0ca89a338a0e8ddfc8841ab175a7e3017214
madani <marwane.madani99@gmail.com>
2023-10-11 14:47:50 -0700

    task 12

---
+++ 0x04-python-more_data_structures/103-python.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+
+/**
+ * print_python_list - printing python list's detail info
+ * @p: py object 
+ */
+void print_python_list(PyObject *p)
+{
+
+}
+
+/**
+ * print_python_bytes - printing bytes of string or the value that was used
+ * @p: py object
+ */
+void print_python_bytes(PyObject *p)
+{
+
+}
--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,8 +2,31 @@
 
 def roman_to_int(roman_string):
     if isinstance(roman_string, str):
-        list_o = [M, D, C, L, X, V, I]
-        
+        list_o = {
+                'M': 1000, 'D': 500, 'C': 100,
+                'L': 50, 'X': 10, 'V': 5, 'I': 1}
+        prob = ['C', 'X', 'I']
+
+        summ = 0
+        len1 = len(roman_string)
+        for i in range(len1):
+            val = 0
+            if (i != len1 - 1) and roman_string[i] in prob:
+                if roman_string[i] == 'C':
+                    if roman_string[i + 1] in ('D', 'M'):
+                        val = 1
+                        summ -= 100
+                elif roman_string[i] == 'X':
+                    if roman_string[i + 1] in ('L', 'C'):
+                        val = 1
+                        summ -= 10
+                elif roman_string[i] == 'I':
+                    if roman_string[i + 1] in ('X', 'V'):
+                        val = 1
+                        summ -= 1
+            if val == 0:
+                summ += list_o[roman_string[i]]
+        return summ
 
     else:
         return 0
